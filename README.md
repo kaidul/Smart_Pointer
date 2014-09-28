66fc8a1d7019aeb18d55558cdaa40fe6ffbd87a6
kaidul <ikaidul@yahoo.com>
2014-09-28 16:19:14 -0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Smart Pointer
 
-Play around with C++ smart pointer to get an intuition on how smart pointer works!
+Smart Pointer Implementation using *Non-intrusive Reference Counting* mechanism.
+
+## Usage
+
+<pre lang="cpp">
+<code>
+SmartPointer<int> foo; // foo = NULL
+SmartPointer<int> bar(new int(7)); // bar = 7;
+
+SmartPointer<int> var = new SmartPointer<int>(new int(4)); // int *var = new int(4);
+
+SmartPointer<std::vector<int> > vectorPtr = new SmartPointer<std::vector<int> >(new vector<int>());
+vectorPtr->push_back(1);
+assert(vectorPtr.at(0) == 1);
+</code>
+</pre>
+
--- main.cpp
+++ main.cpp
@@ -3,17 +3,14 @@
 #include <vector>
 #include "SmartPointer.hpp"
 
-using namespace std;
-
 /* Test Program */
 int main(void) {
-    // heap allocation
+    /*----------------heap allocation--------------------------*/
     SmartPointer<int> *sp1 = new SmartPointer<int>(new int(1));
     SmartPointer<int> *sp2 = new SmartPointer<int>(new int(2));
     SmartPointer<int> *sp3 = new SmartPointer<int>(new int());
     assert(**sp1 != **sp2);
     sp3 = sp1; // like shallow copy
-//    assert(&sp1 != &sp3);
     assert(sp1 == sp3);
     assert(*sp1 == *sp3);
     assert(**sp1 == **sp3);
@@ -26,31 +23,31 @@ int main(void) {
     delete sp2;
     delete sp3;
 
-    // stack allocation
+    /*-------------stack allocation------------- */
     SmartPointer<int> var(new int(7));
     assert(*var == 7);
 
-    SmartPointer<vector<int> > vectorPtr(new vector<int>());
+    /* pointer to complex data types */
+    SmartPointer<std::vector<int> > vectorPtr(new std::vector<int>());
     assert(vectorPtr->empty());
-    vectorPtr->push_back(0);
     vectorPtr->push_back(1);
     vectorPtr->push_back(2);
     vectorPtr->push_back(3);
-    assert(vectorPtr->at(0) == 0);
-    assert(vectorPtr->at(1) == 1);
-    assert(vectorPtr->at(2) == 2);
-    assert(vectorPtr->at(3) == 3);
+    assert(vectorPtr->at(0) == 1);
+    assert(vectorPtr->at(1) == 2);
+    assert(vectorPtr->at(2) == 3);
 
     SmartPointer <int> ptrtoInt(new int(4));
     SmartPointer <int> ptrtoInt2 = ptrtoInt;
     assert(ptrtoInt2 == ptrtoInt);
     assert(*ptrtoInt2 == *ptrtoInt);
 
-    SmartPointer <int> ptrtoInt3;
+    SmartPointer <int> ptrtoInt3; // NULL
     assert(ptrtoInt3 == nullptr);
-    ptrtoInt3 = SmartPointer <int>(new int(2));
+    ptrtoInt3 = SmartPointer <int>(new int(2)); // operator = (T*)
     assert(*ptrtoInt3 == 2);
     SmartPointer <int> ptrtoInt4;
     ptrtoInt4 = ptrtoInt3;
+    std::cout << "Success!\n";
     return 0;
 }
